5381c4e27b9cc495a8b3bebc0b456d3d8aa250c0
Tri Anggi Anggara Saputra <trianggianggara.dev@gmail.com>
2024-07-12 02:14:57 +0700

    feat: Add base repository for all models

---
+++ internal/repository/repository.go
@@ -0,0 +1,79 @@
+package repository
+
+import (
+	"context"
+	res "gh5-backend/pkg/utils/response"
+
+	"github.com/jackc/pgconn"
+	"gorm.io/gorm"
+)
+
+type Repository[T any] struct {
+	conn       *gorm.DB
+	entity     T
+	entityName string
+}
+
+func NewRepository[T any](conn *gorm.DB, entity T, entityName string) *Repository[T] {
+	return &Repository[T]{
+		conn,
+		entity,
+		entityName,
+	}
+}
+
+func (r *Repository[T]) getConn() *gorm.DB {
+	return r.conn
+}
+
+func (m *Repository[T]) maskError(err error) error {
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return res.ErrorBuilder(&res.ErrorConstant.NotFound, err)
+		}
+
+		if pqErr, ok := err.(*pgconn.PgError); ok {
+			if pqErr.Code == "23505" {
+				return res.ErrorBuilder(&res.ErrorConstant.DuplicateEntity, err)
+			}
+		}
+
+		return res.ErrorBuilder(&res.ErrorConstant.UnprocessableEntity, err)
+	}
+
+	return nil
+}
+
+func (r *Repository[T]) Create(ctx context.Context, data T) (T, error) {
+	query := r.conn.Model(r.entity)
+	err := query.Create(&data).Error
+	return data, r.maskError(err)
+}
+
+func (r *Repository[T]) Update(ctx context.Context, data T) (T, error) {
+	query := r.conn.Model(r.entity)
+	err := query.Updates(&data).Error
+	return data, r.maskError(err)
+}
+
+func (r *Repository[T]) Delete(ctx context.Context, entity *T) error {
+	err := r.conn.Model(r.entity).Delete(entity).Error
+	return r.maskError(err)
+}
+
+func (r *Repository[T]) CountByID(ctx context.Context, id any) (int64, error) {
+	var total int64
+	query := r.conn.Model(r.entity)
+	err := query.Model(new(T)).Where("id = ?", id).Count(&total).Error
+	return total, r.maskError(err)
+}
+
+func (r *Repository[T]) FindByID(ctx context.Context, id any) (*T, error) {
+	query := r.conn.Model(r.entity)
+	result := new(T)
+	err := query.Where("id", id).First(result).Error
+	if err != nil {
+		return nil, r.maskError(err)
+	}
+	return result, nil
+}
