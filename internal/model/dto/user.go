f8ca02b35a21d4e7eee54c24d0ec8e9065805e9d
Tri Anggi Anggara Saputra <trianggianggara.dev@gmail.com>
2024-07-12 02:17:00 +0700

    feat: Add common and users dto

---
+++ internal/model/dto/user.go
@@ -0,0 +1,42 @@
+package dto
+
+import (
+	model "gh5-backend/internal/model/entity"
+	res "gh5-backend/pkg/utils/response"
+)
+
+// request
+type (
+	CreateUserRequest struct {
+		Name     string  `json:"name" validate:"required"`
+		Username string  `json:"username" validate:"required"`
+		Email    *string `json:"email,omitempty" validate:"omitempty"`
+		Password string  `json:"password"`
+	}
+)
+
+type (
+	UpdateUserRequest struct {
+		ID       string `param:"id" validate:"required"`
+		Name     string `json:"name"`
+		Email    string `json:"email" validate:"omitempty,email"`
+		Password string `json:"password"`
+	}
+)
+
+type FindByEmailRequest struct {
+	Email string `param:"email" validate:"required"`
+}
+
+// response
+type (
+	UserResponse struct {
+		Data model.UserModel
+	}
+	UserResponseDoc struct {
+		Body struct {
+			Meta res.Meta     `json:"meta"`
+			Data UserResponse `json:"data"`
+		} `json:"body"`
+	}
+)
