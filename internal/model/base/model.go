45affec77b48f39961e936dd0603585ff02ef945
Tri Anggi Anggara Saputra <trianggianggara.dev@gmail.com>
2024-07-12 02:14:08 +0700

    feat: Add base models and user models

---
+++ internal/model/base/model.go
@@ -0,0 +1,29 @@
+package base
+
+import (
+	"gh5-backend/pkg/utils/date"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Entity struct {
+	ID string `json:"id" gorm:"primaryKey;"`
+
+	CreatedAt  time.Time  `json:"created_at"`
+	CreatedBy  string     `json:"created_by"`
+	ModifiedAt *time.Time `json:"modified_at"`
+	ModifiedBy *string    `json:"modified_by"`
+
+	DeletedAt *time.Time `json:"-" gorm:"index"`
+}
+
+func (m *Entity) BeforeUpdate(tx *gorm.DB) (err error) {
+	m.ModifiedAt = date.DateTodayLocal()
+	return
+}
+
+func (m *Entity) BeforeCreate(tx *gorm.DB) (err error) {
+	m.CreatedAt = *date.DateTodayLocal()
+	return
+}
---
+++ internal/model/entity/user.go
@@ -0,0 +1,78 @@
+package entity
+
+import (
+	"context"
+	"os"
+	"time"
+
+	abstraction "gh5-backend/internal/model/base"
+	constant "gh5-backend/pkg/constants"
+	"gh5-backend/pkg/ctxval"
+	"gh5-backend/pkg/utils/date"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type UserEntity struct {
+	Name         string `json:"name" validate:"required" gorm:"size:200; not null"`
+	Username     string `json:"username" validate:"required" gorm:"size:100;not null"`
+	Email        string `json:"email" validate:"required,email" gorm:"index:idx_user_email;unique;size:150;not null"`
+	PasswordHash string `json:"-"`
+	Password     string `json:"password" validate:"required" gorm:"-"`
+	IsActive     bool   `json:"is_active"`
+}
+
+type UserModel struct {
+	abstraction.Entity
+	UserEntity
+	Context context.Context `json:"-" gorm:"-"`
+}
+
+func (UserModel) TableName() string {
+	return "users"
+}
+
+func (m *UserModel) BeforeCreate(tx *gorm.DB) (err error) {
+	m.ID = uuid.New().String()
+	m.IsActive = true
+	m.CreatedAt = *date.DateTodayLocal()
+	m.CreatedBy = constant.DB_DEFAULT_CREATED_BY
+
+	m.HashPassword()
+	m.Password = ""
+	return
+}
+
+func (m *UserModel) BeforeUpdate(tx *gorm.DB) (err error) {
+	m.ModifiedAt = date.DateTodayLocal()
+
+	authCtx := ctxval.GetAuthValue(m.Context)
+	if authCtx != nil {
+		m.ModifiedBy = &authCtx.Name
+	}
+	return
+}
+
+func (m *UserModel) HashPassword() {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(m.Password), bcrypt.DefaultCost)
+	m.PasswordHash = string(bytes)
+}
+
+func (m *UserModel) GenerateJWT() (string, error) {
+	var (
+		jwtKey = os.Getenv(constant.JWT_KEY)
+	)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":    m.ID,
+		"email": m.Email,
+		"name":  m.Name,
+		"exp":   time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(jwtKey))
+	return tokenString, err
+}
